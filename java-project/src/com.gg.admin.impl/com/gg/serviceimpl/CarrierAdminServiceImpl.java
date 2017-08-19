f18ac4e699fed9558d969f2359a5095d050f43ff
GowthamGirithar <g.srirangasamy@sap.com>
2017-08-19 16:43:34 +0530

    added

--- java-project/src/com.gg.admin.impl/com/gg/serviceimpl/CarrierAdminServiceImpl.java
+++ java-project/src/com.gg.admin.impl/com/gg/serviceimpl/CarrierAdminServiceImpl.java
@@ -3,7 +3,7 @@ import com.gg.service.AdminService;
 public class CarrierAdminServiceImpl implements AdminService{
 	public CarrierAdminServiceImpl(){
 	}
-	public void printServiceInfo{
+	public void printServiceInfo(){
 		System.out.println("CarrierAdminServiceImpl Implementation");
 	}
 	
