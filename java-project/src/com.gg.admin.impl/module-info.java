85a48409a3923be8a3657ed44c06bd7ad30d8358
GowthamGirithar <gowthamgirithar@gmail.com>
2017-08-19 18:39:29 +0530

    Update module-info.java

--- java-project/src/com.gg.admin.impl/module-info.java
+++ java-project/src/com.gg.admin.impl/module-info.java
@@ -1,5 +1,5 @@
  module com.gg.admin.impl{
 	 requires transitive com.gg.admin;
-	 exports com.gg.serviceimpl;
+	// exports com.gg.serviceimpl;
 	provides com.gg.service.AdminService with com.gg.serviceimpl.CarrierAdminServiceImpl;
 }
