5e367a6308c0f69ed499d7867fa0d435e568505a
GowthamGirithar <gowthamgirithar@gmail.com>
2017-08-19 18:49:07 +0530

    Update Client.java

--- java-project/src/com.gg.admin.client/com/gg/client/Client.java
+++ java-project/src/com.gg.admin.client/com/gg/client/Client.java
@@ -1,6 +1,9 @@
 package com.gg.client;
 import com.gg.service.AdminService;
-
+/**
+by Gowtham Girithar Srirangasamy
+This class is client which is consuming the implemenation of a Admin Service.
+**/
 public class Client {
 	public static void main(String[] args){
 		
