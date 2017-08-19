017dbe5fba6dec002edb12ce83023bbd117c1534
GowthamGirithar <g.srirangasamy@sap.com>
2017-08-19 16:55:28 +0530

    added

--- java-project/src/com.gg.admin.impl/module-info.java
+++ java-project/src/com.gg.admin.impl/module-info.java
@@ -1,5 +1,5 @@
  module com.gg.admin.impl{
-	 requires transitive com.gg.admin;
+	 //requires transitive com.gg.admin;
 	 exports com.gg.serviceimpl;
 	provides com.gg.service.AdminService with com.gg.serviceimpl.CarrierAdminServiceImpl;
 }
